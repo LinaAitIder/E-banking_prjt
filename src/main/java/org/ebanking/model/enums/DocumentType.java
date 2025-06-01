0ca8865a5ac460691cdbe64350380954dfe8317f
Lina Ait Ider <103391128+LinaAitIder@users.noreply.github.com>
2025-06-01 14:55:15 +0100

    Revert "fixes"

--- src/main/java/org/ebanking/model/enums/DocumentType.java
+++
@@ -1,8 +0,0 @@
-package org.ebanking.model.enums;
-
-public enum DocumentType {
-    ID_PROOF,
-    ADDRESS_PROOF,
-    INCOME_PROOF,
-    PHOTO
-}
--- src/main/java/org/ebanking/model/enums/EnrollmentStatus.java
+++
@@ -1,9 +0,0 @@
-package org.ebanking.model.enums;
-
-public enum EnrollmentStatus {
-    PENDING,
-    APPROVED,
-    REJECTED,
-    UNDER_REVIEW,
-    COMPLETED
-}
