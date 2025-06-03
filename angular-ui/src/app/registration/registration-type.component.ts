edadbbe44706ccd77ee25bd8378fea73203350a3
Lina Ait Ider <103391128+LinaAitIder@users.noreply.github.com>
2025-06-03 07:52:24 +0100

    Revert "admin acount can be created, admin can log in and change password"

--- angular-ui/src/app/registration/registration-type.component.ts
+++ angular-ui/src/app/registration/registration-type.component.ts
@@ -1,34 +1,30 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "../home/navbar/navbar.component";
-import { CommonModule } from '@angular/common';
+import {Router, RouterOutlet} from '@angular/router';
+import {NavbarComponent} from "../home/navbar/navbar.component";
 
 @Component({
-  selector: 'app-registration-type',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    NavbarComponent,
-    CommonModule
-  ],
-  templateUrl: './registration-type.component.html',
-  styleUrls: ['./registration-type.component.scss']
+    selector: 'app-registration-type',
+    templateUrl: './registration-type.component.html',
+    imports: [
+        RouterOutlet,
+        NavbarComponent
+    ],
+    styleUrls: ['./registration-type.component.scss']
 })
 export class RegistrationTypeComponent {
-  constructor(private router: Router) {}
+    constructor(private router: Router) {}
 
-  selectUserType(userType: string) {
-    switch(userType) {
-      case 'client':
-        this.router.navigate(['/register/client']);
-        break;
-      case 'agent':
-        this.router.navigate(['/register/agent']);
-        break;
-      case 'admin':
-        // Redirection vers la page de login admin plutôt que d'inscription
-        this.router.navigate(['/admin/login']);
-        break;
+    selectUserType(userType: string) {
+        switch(userType) {
+            case 'client':
+                this.router.navigate(['clientRegistration']);
+                break;
+            case 'agent':
+                this.router.navigate(['agentRegistration']);
+                break;
+            case 'admin':
+                this.router.navigate(['adminRegistration']);
+                break;
             default:
                 this.router.navigate(['registration']);
         }
