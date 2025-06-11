5f79a2330956416508786ceae5b661af1ffc9e17
LinaAitIder <linaitider@gmail.com>
2025-06-11 04:08:08 +0100

    Attempt to fix nginx welcome file prblm

--- angular-ui/src/main.ts
+++ angular-ui/src/main.ts
@@ -2,16 +2,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {AppComponent} from './app/app.component';
-import {provideRouter} from "@angular/router";
+import {provideRouter, withHashLocation} from "@angular/router";
 import {routes} from "./app/app.routes";
 import {JwtInterceptor} from "./app/utils/jwt.interceptor";
-
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 
 bootstrapApplication(AppComponent, {
-  providers: [
-      provideHttpClient(
-      withInterceptors([
-        JwtInterceptor
-      ])
-  ),provideHttpClient(), provideRouter(routes)],
+    providers: [
+        provideRouter(routes, withHashLocation()),
+        provideHttpClient(withInterceptors([JwtInterceptor])),
+    ],
 });
